45fd434cfa2e8693755e342fbe6620564ec6fad6
NadezhdaT7 <nadejdatukmakova@gmail.com>
2024-11-15 12:31:49 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,7 @@
+count_of_exercises = 12
+count_of_huors = 1.5
+name_of_the_course = 'Python'
+time_for_one_exercise = count_of_huors/count_of_exercises
+print("Курс:", name_of_the_course, ",", " Всего задач:",count_of_exercises,
+      ",", " Затрачено часов:", count_of_huors, ",", "среднее время выполнения:",
+      time_for_one_exercise)
